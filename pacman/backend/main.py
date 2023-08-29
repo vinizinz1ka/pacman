adb65dbd641f9fac4326a8125ba1fa74042bfc3b
vinizinz1ka <111133013+vinizinz1ka@users.noreply.github.com>
2023-08-28 21:18:53 -0300

    Delete pacman directory

--- pacman/backend/main.py
+++
@@ -1,7 +0,0 @@
-from config import *
-from rotas import *
-
-with app.app_context():
-    db.create_all()
-    CORS(app)
-    app.run(debug=True, host="0.0.0.0")
--- pacman/criar_tabelas.py
+++
@@ -1,8 +0,0 @@
-from config import *
-from classes import *
-if os.path.exists(arquivobd):
-    os.remove(arquivobd)
-    
-db.create_all()
-
-print("Tabelas criadas")
